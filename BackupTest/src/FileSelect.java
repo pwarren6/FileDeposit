35a7111892ca49cc26da0bc36bb272e87c13beb9
pwarren6 <warren46892@gmail.com>
2015-10-07 20:58:24 -0400

    New classes created.

--- BackupTest/src/FileSelect.java
+++ BackupTest/src/FileSelect.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-public class FileChooserDemo extends JPanel
-implements ActionListener{
+public class FileSelect extends JPanel implements ActionListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static private final String newline = "\n";
 	JButton open, save;
 	JTextArea log;
 	JFileChooser fc;
-	public FileChooserDemo(){
+	public FileSelect(){
 		super(new BorderLayout());
 		log = new JTextArea(5,20);
 		log.setMargin(new Insets(5,5,5,5));
@@ -21,13 +18,13 @@ implements ActionListener{
 		JScrollPane logScroll = new JScrollPane(log);
 		
 		fc = new JFileChooser();
-		
-		open = new JButton("Open a File",
+		open = new JButton("Open...",
 				createImageIcon("images/Open16.gif"));
 		open.addActionListener(this);
 		
-		save = new JButton("Save a File",
+		save = new JButton("Save...",
 				createImageIcon("images/Save16.gif"));
+		save.addActionListener(this);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(open);
@@ -38,24 +35,24 @@ implements ActionListener{
 	}
 	public void actionPerformed(ActionEvent e){
 		if (e.getSource() == open){
-			int returnVal = fc.showOpenDialog(FileChooserDemo.this);
+			int returnVal = fc.showOpenDialog(FileSelect.this);
 			
 			if(returnVal == JFileChooser.APPROVE_OPTION){
 				File file = fc.getSelectedFile();
-				log.append("You have selected: " + file.getName() + "." + newline);
+				log.append("You have selected: " + file.getName() + "." + "\n");
 			}else{
-				log.append("Selection cancelled." + newline);
+				log.append("Selection cancelled." + "\n");
 			}
 			log.setCaretPosition(log.getDocument().getLength());
 		}
 		if (e.getSource() == save){
-			int returnVal = fc.showOpenDialog(FileChooserDemo.this);
+			int returnVal = fc.showSaveDialog(FileSelect.this);
 			
 			if(returnVal == JFileChooser.APPROVE_OPTION){
 				File file = fc.getSelectedFile();
-				log.append("You are now saving: " + file.getName() + "." + newline);
+				log.append("You are now saving: " + file.getName() + "." + "\n");
 			}else{
-				log.append("Save command cancelled." + newline);
+				log.append("Save command cancelled." + "\n");
 			}
 			log.setCaretPosition(log.getDocument().getLength());
 		}
@@ -70,10 +67,10 @@ implements ActionListener{
 		}
 	}
 	private static void createAndShowGUI(){
-		JFrame frame = new JFrame("FileChooserDemo");
+		JFrame frame = new JFrame("FileSelect");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.add(new FileChooserDemo());
+		frame.add(new FileSelect());
 		frame.pack();
 		frame.setVisible(true);
 	}
@@ -86,3 +83,4 @@ implements ActionListener{
 		});
 	}
 }
+
--- BackupTest/src/FolderSelect.java
+++ BackupTest/src/FolderSelect.java
@@ -2,18 +2,15 @@ import java.io.*;
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
-
-public class FileChooserDemo extends JPanel
-implements ActionListener{
+public class FolderSelect extends JPanel implements ActionListener {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	static private final String newline = "\n";
 	JButton open, save;
 	JTextArea log;
 	JFileChooser fc;
-	public FileChooserDemo(){
+	public FolderSelect(){
 		super(new BorderLayout());
 		log = new JTextArea(5,20);
 		log.setMargin(new Insets(5,5,5,5));
@@ -21,13 +18,14 @@ implements ActionListener{
 		JScrollPane logScroll = new JScrollPane(log);
 		
 		fc = new JFileChooser();
-		
-		open = new JButton("Open a File",
+		fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+		open = new JButton("Open...",
 				createImageIcon("images/Open16.gif"));
 		open.addActionListener(this);
 		
-		save = new JButton("Save a File",
+		save = new JButton("Save...",
 				createImageIcon("images/Save16.gif"));
+		save.addActionListener(this);
 		
 		JPanel buttonPanel = new JPanel();
 		buttonPanel.add(open);
@@ -38,24 +36,24 @@ implements ActionListener{
 	}
 	public void actionPerformed(ActionEvent e){
 		if (e.getSource() == open){
-			int returnVal = fc.showOpenDialog(FileChooserDemo.this);
+			int returnVal = fc.showOpenDialog(FolderSelect.this);
 			
 			if(returnVal == JFileChooser.APPROVE_OPTION){
 				File file = fc.getSelectedFile();
-				log.append("You have selected: " + file.getName() + "." + newline);
+				log.append("You have selected: " + file.getName() + "." + "\n");
 			}else{
-				log.append("Selection cancelled." + newline);
+				log.append("Selection cancelled." + "\n");
 			}
 			log.setCaretPosition(log.getDocument().getLength());
 		}
 		if (e.getSource() == save){
-			int returnVal = fc.showOpenDialog(FileChooserDemo.this);
+			int returnVal = fc.showSaveDialog(FolderSelect.this);
 			
 			if(returnVal == JFileChooser.APPROVE_OPTION){
 				File file = fc.getSelectedFile();
-				log.append("You are now saving: " + file.getName() + "." + newline);
+				log.append("You are now saving: " + file.getName() + "." + "\n");
 			}else{
-				log.append("Save command cancelled." + newline);
+				log.append("Save command cancelled." + "\n");
 			}
 			log.setCaretPosition(log.getDocument().getLength());
 		}
@@ -70,10 +68,10 @@ implements ActionListener{
 		}
 	}
 	private static void createAndShowGUI(){
-		JFrame frame = new JFrame("FileChooserDemo");
+		JFrame frame = new JFrame("FolderSelect");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		frame.add(new FileChooserDemo());
+		frame.add(new FolderSelect());
 		frame.pack();
 		frame.setVisible(true);
 	}
